1167bdee732b7796c534bf58dc572d5a1baa45dd
khoramshahi <m80.khoramshahi@gmail.com>
2017-10-25 18:03:08 +0200

    parameterize the calibration

--- include/ObjectTracker.h
+++ include/ObjectTracker.h
@@ -18,11 +18,35 @@ private:
 	ros::Rate loop_rate_;
 
 	tf::TransformListener listener_;
+	tf::TransformBroadcaster brodcaster_;
+
+	std::string frame_robot_static_;
+	std::string frame_robot_calibration_;
+	std::string frame_mocap_static_;
+	std::string frame_mocap_object_;
+	std::string frame_robot_object_;
+	std::string frame_robot_base_;
+
+	tf::Quaternion quat_offset_;
+
+	tf::StampedTransform transform_static_base_;
+
+
+	ros::Duration time_diff_;
 
 
 public:
 	ObjectTracker(ros::NodeHandle &n,
-	                      double frequency);
+	              double frequency,
+	              std::string frame_robot_static,
+	              std::string frame_robot_calibration,
+	              std::string frame_robot_object,
+	              std::string frame_robot_base,
+	              std::string frame_mocap_static,
+	              std::string frame_mocap_object);
+
+
+	void Run();
 
 
 private:
--- src/ObjectTracker.cpp
+++ src/ObjectTracker.cpp
@@ -3,83 +3,89 @@
 
 ObjectTracker::ObjectTracker(
     ros::NodeHandle &n,
-    double frequency)
+    double frequency,
+    std::string frame_robot_static,
+    std::string frame_robot_calibration,
+    std::string frame_robot_object,
+    std::string frame_robot_base,
+    std::string frame_mocap_static,
+    std::string frame_mocap_object)
 	: nh_(n),
-	  loop_rate_(frequency) {
-
+	  loop_rate_(frequency),
+	  frame_robot_static_(frame_robot_static),
+	  frame_robot_calibration_(frame_robot_calibration),
+	  frame_robot_object_(frame_robot_object),
+	  frame_robot_base_(frame_robot_base),
+	  frame_mocap_static_(frame_mocap_static),
+	  frame_mocap_object_(frame_mocap_object) {
 
 	ROS_INFO_STREAM("ObjectTracker is created at: " << nh_.getNamespace() << " with freq: " << frequency << "Hz");
 
-
-	static tf::TransformBroadcaster br;
-	tf::StampedTransform transform;
-
-	tf::StampedTransform transform_corner_calibration;
-	tf::StampedTransform transform_corner_object;
 	tf::StampedTransform transform_base_link;
-	tf::StampedTransform transform_corner_base;
+	tf::StampedTransform transform_static_calibration;
+	tf::StampedTransform transform_static_object;
+
+	bool ready_calibration = false;
+	bool ready_mocap = false;
+	bool ready_base = false;
+	bool ready_static = false;
+
+	quat_offset_ = tf::createQuaternionFromRPY(0, 0, 0);
 
 
-	ROS_INFO("Calibration: put the object-marker on top of the imu-sensor and press Enter");
+	ROS_INFO_STREAM("Calibration: match the object-marker with " << frame_robot_calibration_ << " in rviz and press Enter");
 	std::cin.get();
 
-	ros::Duration time_diff;
 
-
-	bool robots_tf_ready = false;
-	bool mocap_tf_ready  = false;
-	bool time_robot      = false;
-bool corner_base_ready = false;
-
-	while (nh_.ok() && (!robots_tf_ready || !mocap_tf_ready || !time_robot || !corner_base_ready) ) {
-
+	while (nh_.ok() && (!ready_calibration || !ready_mocap || !ready_base || !ready_static) ) {
 
 		try {
-			listener_.lookupTransform("top_right_corner", "top_left_corner",
-			                          ros::Time(0), transform_corner_calibration);
-			robots_tf_ready = true;
-		}
-		catch (tf::TransformException ex) {
-			ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: " << "top_right_corner" << " to: " << "top_left_corner" );
-		}
-
-		try {
-			listener_.lookupTransform("mocap_top_right_corner", "mocap_object",
-			                          ros::Time(0), transform_corner_object);
-			mocap_tf_ready = true;
-		}
-		catch (tf::TransformException ex) {
-			ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: " << "mocap_top_right_corner" << " to: " << "mocap_object" );
-		}
-
-
-		try {
-			listener_.lookupTransform("base_link", "base_link",
+			listener_.lookupTransform(frame_robot_base_, frame_robot_base_,
 			                          ros::Time(0), transform_base_link);
 
-			time_robot = true;
+			ready_base = true;
 
-			time_diff = transform_base_link.stamp_ - ros::Time::now();
-			ROS_INFO_STREAM("Time difference:" << time_diff );
+			time_diff_ = transform_base_link.stamp_ - ros::Time::now();
+			ROS_WARN_STREAM_THROTTLE(10,"Time difference:" << time_diff_ );
 
 		}
 		catch (tf::TransformException ex) {
-			ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: " << "top_right_corner" << " to: " << "top_left_corner" );
+			ROS_WARN_STREAM_THROTTLE(1, "Waiting for base_link frame." );
+		}
+
+		try {
+			listener_.lookupTransform(frame_robot_static_, frame_robot_base_,
+			                          ros::Time(0), transform_static_base_);
+
+			ready_static = true;
+
+		}
+		catch (tf::TransformException ex) {
+			ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: " << frame_robot_static_ << " to: " << "base" );
 		}
 
 
 		try {
-			listener_.lookupTransform("top_right_corner", "base_link",
-			                          ros::Time(0), transform_corner_base);
-
-			corner_base_ready = true;
-
+			listener_.lookupTransform(frame_robot_static_, frame_robot_calibration_,
+			                          ros::Time(0), transform_static_calibration);
+			ready_calibration = true;
 		}
 		catch (tf::TransformException ex) {
-			ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: " << "top_right_corner" << " to: " << "base" );
+			ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: " <<
+			                         frame_robot_static_ << " to: "
+			                         << frame_robot_calibration_ );
 		}
 
-
+		try {
+			listener_.lookupTransform(frame_mocap_static_, frame_mocap_object_,
+			                          ros::Time(0), transform_static_object);
+			ready_mocap = true;
+		}
+		catch (tf::TransformException ex) {
+			ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: "
+			                         << frame_mocap_static_ << " to: "
+			                         << frame_mocap_object_ );
+		}
 
 		ros::spinOnce();
 		loop_rate_.sleep();
@@ -87,71 +93,91 @@ bool corner_base_ready = false;
 	}
 
 
-
-	tf::Vector3 origin2 = transform_corner_calibration.getOrigin();
-	tf::Vector3 origin1 = transform_corner_object.getOrigin();
+	tf::Vector3 origin2 = transform_static_calibration.getOrigin();
+	tf::Vector3 origin1 = transform_static_object.getOrigin();
 
 	tf::Vector3 xyz = origin1.cross(origin2);
 	xyz.normalized();
 	double w = 1.0 * acos(origin1.dot(origin2) / (origin1.length() * origin2.length()));
 
-
-	tf::Quaternion quat_offset(xyz, w);
-	quat_offset.normalize();
-
-
+	tf::Quaternion q(xyz, w);
+	q.normalize();
+	quat_offset_ = q;
 
 
 
-	ROS_INFO("Calibraton finished");
+	ROS_INFO_STREAM("Calibraton is finished. Quaternion offset" << quat_offset_);
+
+	// tf::StampedTransform transform;
 
 
-	std::string from_frame = "mocap_top_right_corner";
-	std::string to_frame = "mocap_object";
+	// while (nh_.ok()) {
+
+	// 	try {
+	// 		listener_.lookupTransform(frame_mocap_static_, frame_mocap_object_,
+	// 		                          ros::Time(0), transform);
+
+	// 		tf::Vector3 correct_position = tf::quatRotate(quat_offset_, transform.getOrigin());
+
+	// 		transform.setOrigin(correct_position);
+
+	// 		// br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), frame_robot_static_, "object"));
+	// 		transform.setData(transform_static_base_.inverse() * transform );
+
+	// 		transform.stamp_ = ros::Time::now() + time_diff_;
+	// 		transform.frame_id_ = frame_robot_base_;
+	// 		transform.child_frame_id_ = frame_robot_object_;
+	// 		brodcaster_.sendTransform(transform);
+
+	// 	}
+	// 	catch (tf::TransformException ex) {
+	// 		ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: "
+	// 		                         << frame_mocap_static_ << " to: "
+	// 		                         << frame_mocap_object_ );
+	// 	}
+
+	// 	ros::spinOnce();
+	// 	loop_rate_.sleep();
+
+	// }
+
+}
+
+
+void ObjectTracker::Run() {
+
+	tf::StampedTransform transform;
+
+
 
 	while (nh_.ok()) {
 
-
 		try {
-			listener_.lookupTransform(from_frame, to_frame,
+			listener_.lookupTransform(frame_mocap_static_, frame_mocap_object_,
 			                          ros::Time(0), transform);
 
-
-			tf::Vector3 correct_position = tf::quatRotate(quat_offset, transform.getOrigin());
-
+			tf::Vector3 correct_position = tf::quatRotate(quat_offset_, transform.getOrigin());
 
 			transform.setOrigin(correct_position);
 
+			// br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), frame_robot_static_, "object"));
+			transform.setData(transform_static_base_.inverse() * transform );
 
-
-			// br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "top_right_corner", "object"));
-			transform.setData(transform_corner_base.inverse() * transform );
-
-			transform.stamp_ = ros::Time::now() + time_diff;
-			transform.frame_id_ = "base_link";
-			transform.child_frame_id_ = "object";
-			br.sendTransform(transform);
+			transform.stamp_ = ros::Time::now() + time_diff_;
+			transform.frame_id_ = frame_robot_base_;
+			transform.child_frame_id_ = frame_robot_object_;
+			brodcaster_.sendTransform(transform);
 
 		}
 		catch (tf::TransformException ex) {
-			// rotation_matrix.setZero();
-			ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: " << from_frame << " to: " << to_frame );
-			// return false;
+			ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: "
+			                         << frame_mocap_static_ << " to: "
+			                         << frame_mocap_object_ );
 		}
-
-
-
 
 		ros::spinOnce();
 		loop_rate_.sleep();
 
 	}
-
-
-
-
-
 }
-
-
 
