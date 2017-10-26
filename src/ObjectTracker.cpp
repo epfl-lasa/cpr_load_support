d714e8ffe2b3f9851d4c1a282887c8f3ed89802c
khoramshahi <m80.khoramshahi@gmail.com>
2017-10-26 20:35:30 +0200

    clean the code, remove debuging

--- src/ObjectTracker.cpp
+++ src/ObjectTracker.cpp
@@ -1,6 +1,4 @@
 #include "ObjectTracker.h"
-#include "geometry_msgs/PointStamped.h"
-
 
 ObjectTracker::ObjectTracker(
     ros::NodeHandle &n,
@@ -26,10 +24,11 @@ ObjectTracker::ObjectTracker(
 
 	ROS_INFO_STREAM("ObjectTracker is created at: " << nh_.getNamespace() << " with freq: " << frequency << "Hz");
 
-	// tf::StampedTransform transform_base_link;
-	// tf::StampedTransform transform_static_calibration;
-	// tf::StampedTransform transform_static_object;
-
+	tf::StampedTransform transform_temp;
+	tf::StampedTransform transform_ee_pbase;
+	tf::StampedTransform transform_pbase_calib;
+	tf::StampedTransform transform_mocap_ee_calib;
+	tf::StampedTransform transform_robot_ee_calib;
 
 	bool ready_base = false;
 	bool ready_endeffector = false;
@@ -39,23 +38,17 @@ ObjectTracker::ObjectTracker(
 	quat_offset_ = tf::createQuaternionFromRPY(0, 0, 0);
 
 
-	ROS_INFO_STREAM("Calibration: make sure markers are visibles: "
+	ROS_INFO_STREAM("Make sure the z-axis for both "
+	                << frame_mocap_endeffector_ << " and "
+	                << frame_robot_endeffector_ << " is pointing up.");
+
+	ROS_INFO_STREAM("Make sure markers are visibles: "
 	                << frame_mocap_calibration_ << " and "
 	                << frame_mocap_endeffector_);
 
-	ROS_WARN_STREAM("Make sure the z-axis for both"
-	                << frame_mocap_endeffector_ << " and "
-	                << frame_robot_endeffector_ << " is pointing up.");
+	ROS_INFO("If ready, press ENTER ...");
+
 	std::cin.get();
-
-
-	tf::StampedTransform transform_temp;
-	tf::StampedTransform transform_ee_pbase;
-	tf::StampedTransform transform_pbase_calib;
-
-	tf::StampedTransform transform_mocap_ee_calib;
-	tf::StampedTransform transform_robot_ee_calib;
-
 
 
 	while (nh_.ok() && (!ready_base || !ready_endeffector || !ready_calibration || !ready_mocap)) {
@@ -109,58 +102,21 @@ ObjectTracker::ObjectTracker(
 		loop_rate_.sleep();
 	}
 
-
+	// this transformation cannot be listened to directly due to time difference problems
 	transform_robot_ee_calib.setData(transform_ee_pbase * transform_pbase_calib);
-
-
-
-	// while (nh_.ok()) {
-	// 		transform_robot_ee_calib.stamp_ = ros::Time::now() + time_diff_;
-	// 		transform_robot_ee_calib.frame_id_ = frame_robot_endeffector_;
-	// 		transform_robot_ee_calib.child_frame_id_ = "fake";
-	// 		brodcaster_.sendTransform(transform_robot_ee_calib);
-	// 	ros::spinOnce();
-	// 	loop_rate_.sleep();
-	// }
-
-
-
-
 
 
 	tf::Vector3 origin1 = transform_mocap_ee_calib.getOrigin();
 	tf::Vector3 origin2 = transform_robot_ee_calib.getOrigin();
 
 
-	ROS_INFO_STREAM("in robot: " << origin1.getX() << " " << origin1.getY() << " " << origin1.getZ() );
-	ROS_INFO_STREAM("in mocap: " << origin2.getX() << " " << origin2.getY() << " " << origin2.getZ() );
-
-	// ros::Publisher pub_target_ = nh_.advertise<geometry_msgs::PointStamped>("my_point", 1);
-
-	// geometry_msgs::PointStamped msg;
-
-	// msg.header.frame_id = frame_mocap_endeffector_;
-	// msg.point.x = origin2.getX();
-	// msg.point.y = origin2.getY();
-	// msg.point.z = origin2.getZ();
-
-	// while (nh_.ok()) {
-
-	// 	msg.header.stamp = ros::Time::now();
-	// 	pub_target_.publish(msg);
-
-	// 	ros::spinOnce();
-	// 	loop_rate_.sleep();
-	// }
-
-
-	if(origin1.getZ()*origin2.getZ() < 0){
+	if (origin1.getZ()*origin2.getZ() < 0) {
 		ROS_WARN("It seems that the z-axes are not matching!");
+		ROS_INFO_STREAM("calibration point in robot: " << origin1.getX() << " " << origin1.getY() << " " << origin1.getZ() );
+		ROS_INFO_STREAM("calibration point in mocap: " << origin2.getX() << " " << origin2.getY() << " " << origin2.getZ() );
 	}
 
-
-
-// assuming the two frames matching in z-axis
+	// assuming the two frames matching in z-axis
 	origin1.setZ(0);
 	origin2.setZ(0);
 
@@ -169,89 +125,11 @@ ObjectTracker::ObjectTracker(
 	xyz.normalized();
 	double w = 1.0 * acos(origin1.dot(origin2) / (origin1.length() * origin2.length()));
 
-	ROS_INFO_STREAM("Calibraton is finished. rotation: " << w
-	                << " around " << xyz.getX() << " " << xyz.getY() << " " << xyz.getZ() );
-
-
 	tf::Quaternion q(xyz, w);
 	q.normalize();
 	quat_offset_ = q;
 
-	ROS_INFO_STREAM("Calibraton is finished. Quaternion offset: " << quat_offset_.getAngleShortestPath() << "[rad]");
-
-
-	// while (nh_.ok() && (!ready_calibration || !ready_mocap || !ready_base || !ready_static) ) {
-
-	// 	try {
-	// 		listener_.lookupTransform(frame_robot_base_, frame_robot_base_,
-	// 		                          ros::Time(0), transform_base_link);
-
-	// 		ready_base = true;
-
-	// 		time_diff_ = transform_base_link.stamp_ - ros::Time::now();
-	// 		ROS_WARN_STREAM_THROTTLE(10, "Time difference:" << time_diff_ );
-
-	// 	}
-	// 	catch (tf::TransformException ex) {
-	// 		ROS_WARN_STREAM_THROTTLE(1, "Waiting for base_link frame." );
-	// 	}
-
-	// 	try {
-	// 		listener_.lookupTransform(frame_robot_static_, frame_robot_base_,
-	// 		                          ros::Time(0), transform_static_base_);
-
-	// 		ready_static = true;
-
-	// 	}
-	// 	catch (tf::TransformException ex) {
-	// 		ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: " << frame_robot_static_ << " to: " << "base" );
-	// 	}
-
-
-	// 	try {
-	// 		listener_.lookupTransform(frame_robot_static_, frame_robot_calibration_,
-	// 		                          ros::Time(0), transform_static_calibration);
-	// 		ready_calibration = true;
-	// 	}
-	// 	catch (tf::TransformException ex) {
-	// 		ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: " <<
-	// 		                         frame_robot_static_ << " to: "
-	// 		                         << frame_robot_calibration_ );
-	// 	}
-
-	// 	try {
-	// 		listener_.lookupTransform(frame_mocap_static_, frame_mocap_object_,
-	// 		                          ros::Time(0), transform_static_object);
-	// 		ready_mocap = true;
-	// 	}
-	// 	catch (tf::TransformException ex) {
-	// 		ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: "
-	// 		                         << frame_mocap_static_ << " to: "
-	// 		                         << frame_mocap_object_ );
-	// 	}
-
-	// 	ros::spinOnce();
-	// 	loop_rate_.sleep();
-
-	// }
-
-
-	// tf::Vector3 origin2 = transform_static_calibration.getOrigin();
-	// tf::Vector3 origin1 = transform_static_object.getOrigin();
-
-	// tf::Vector3 xyz = origin1.cross(origin2);
-	// xyz.normalized();
-	// double w = 1.0 * acos(origin1.dot(origin2) / (origin1.length() * origin2.length()));
-
-	// tf::Quaternion q(xyz, w);
-	// q.normalize();
-	// quat_offset_ = q;
-
-
-
-	// ROS_INFO_STREAM("Calibraton is finished. Quaternion offset" << quat_offset_);
-
-
+	ROS_INFO_STREAM("Calibraton is finished. Rotation offset: " << quat_offset_.getAngleShortestPath() << "[rad]");
 
 }
 
@@ -259,10 +137,23 @@ ObjectTracker::ObjectTracker(
 void ObjectTracker::Run() {
 
 	tf::StampedTransform transform;
+	tf::StampedTransform transform_armbase_endeffector;
 
+	transform.setIdentity();
+	transform_armbase_endeffector.setIdentity();
 
 
 	while (nh_.ok()) {
+
+		try {
+			listener_.lookupTransform(frame_arm_base_, frame_robot_endeffector_,
+			                          ros::Time(0), transform_armbase_endeffector);
+		}
+		catch (tf::TransformException ex) {
+			ROS_WARN_STREAM_THROTTLE(1, "Waiting for TF from: " <<
+			                         frame_arm_base_ << " to: "
+			                         << frame_robot_endeffector_);
+		}
 
 		try {
 			listener_.lookupTransform(frame_mocap_endeffector_, frame_mocap_object_,
@@ -273,10 +164,10 @@ void ObjectTracker::Run() {
 			transform.setOrigin(correct_position);
 
 			// br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), frame_robot_static_, "object"));
-			// transform.setData(transform_static_base_.inverse() * transform );
+			transform.setData(transform_armbase_endeffector * transform );
 
 			transform.stamp_ = ros::Time::now() + time_diff_;
-			transform.frame_id_ = frame_robot_endeffector_;
+			transform.frame_id_ = frame_arm_base_;
 			transform.child_frame_id_ = frame_robot_object_;
 			brodcaster_.sendTransform(transform);
 
